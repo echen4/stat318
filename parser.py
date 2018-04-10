c94b00af7f4bd49a26d10c592c0addee98d6d023
Alex <thefamily.guo@gmail.com>
2018-04-09 22:11:46 -0400

    .csv file parser function added

--- parser.py
+++ parser.py
@@ -1,4 +1,12 @@
+"""
+STAT 318 Final Project
+Team Rocket Is Blasting Off Again!
+Evelyn Chen & Alexandria Guo
+"""
+
+import sys
 import json
+import csv
 
 def parser(filename):
 	data = {}
@@ -16,7 +24,31 @@ def writeJSON(filename, data):
 	with open(filename, 'w') as f:
 		json.dump(data, f)
 
-def main():
-	writeJSON("gen7anythinggoes-1760.json", parser("gen7anythinggoes-1760.txt"))
+def load_csv(csv_file):
+    """given a CSV file where each row is a data point,
+    with the first column being user/item id, the second column being the
+    item/user id, and the third column being the rating, return a dictonary
+    with the first column of user/item as keys to dictionaries with the second
+    column item/user as keys for their rating values
+    """
+    reader = csv.reader(open(csv_file, 'r'))
+    pkmn_directory = {}
+    headers = next(reader);
+    print(headers);
+    # PKMN#, Name, Type1, Type2, Total, HP, Attack, Defense, Sp.Atk, Sp.Def, Speed, Generation, Legendary
+    for dp in reader:
+        if (dp[1] in pkmn_directory):
+            print("Adding " + dp[1] + "again")
+        else:
+            pkmn_directory[dp[1]] = [dp[0]] + list(dp[2:len(dp)])
+    return pkmn_directory
 
-main()
+def main(txt_data, csv_data, json_out):
+	writeJSON(json_out, parser(txt_data))
+    pkmn_directory1 = load_csv(csv_file)
+
+if __name__=='__main__':
+    if len(sys.argv)!=2:
+        print('Usage: python data_loading.py txt_data csv_data json_out')
+    else:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
